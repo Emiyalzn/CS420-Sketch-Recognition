0c11c3a6da3af3ca74b4016280821b4fbfc40a5b
Emiyali <Emiyalzn@gmail.com>
2022-06-16 10:13:36 +0800

    add grad clipping for rnn

--- run/rnn_train.py
+++ run/rnn_train.py
@@ -62,7 +62,7 @@ class SketchRNNTrain(BaseTrain):
             loss = criterion(logits, categories)
             if is_train:
                 loss.backward()
-                nn.utils.clip_grad_value_(model.params(), 1.0) # gradient clipping
+                nn.utils.clip_grad_value_(model.params(True), 1.0) # gradient clipping
                 optimizer.step()
 
         return logits, loss, categories
